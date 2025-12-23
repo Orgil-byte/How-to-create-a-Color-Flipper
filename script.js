42d14eea67449569410c5f0a46e5fead87ad1edc
U.Orgil <ulziitogtohorgil1908@gmail.com>
2025-12-23 13:28:31 +0800

    DONE ALMOST

--- script.js
+++ script.js
@@ -1,13 +1,48 @@
 const btn = document.getElementById(`btn`);
 const displayColorName = document.querySelector(`.generator-color`);
-const colorLabel = document.querySelector(`.color-label`);
-const hexLabel = document.querySelector(`.hex-label`);
+const simpleCat = document.querySelector(`.hex-label`);
+const hexCat = document.querySelector(`.hex`);
 const body = document.querySelector(`.center`);
+const colorNames = [
+  `red`,
+  `blue`,
+  `black`,
+  `cyan`,
+  `white`,
+  `brown`,
+  `green`,
+  `yellow`,
+  `orange`,
+  `purple`,
+  `gray`,
+  `pink`,
+  `teal`,
+  `maroon`,
+  `gold`,
+  `silver`,
+  `turquoise`,
+  `navy blue`,
+  `magenta`,
+  `olive`,
+];
 
-const colorChange = () => {
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  displayColorName.textContent = `#` + randomColor;
-  body.style.backgroundColor = `#` + randomColor;
+const colorChange = (id) => {
+  let randomColor;
+  if (id === 1) {
+    randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    displayColorName.textContent = `#` + randomColor;
+    body.style.backgroundColor = `#` + randomColor;
+    return;
+  }
+  if (id === 2) {
+    const randomNum = Math.floor(Math.random() * colorNames.length);
+    randomColor = colorNames[randomNum];
+    displayColorName.textContent = " " + randomColor;
+    body.style.backgroundColor = " " + randomColor;
+    return;
+  }
 };
 
+hexCat.addEventListener(`click`, colorChange(1));
+simpleCat.addEventListener(`click`, colorChange(2));
 btn.addEventListener(`click`, colorChange);
